5052708a34dce2e60b1eec4b0f4d208259e80fbc
Osimitzu <angel_aocc@hotmail.com>
2023-10-22 02:40:55 -0600

    nav and home components completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Osiris Contreras
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm currently studying Software Development at the Instituto de
+          Estudios Universitarios. I have recently completed the full-stack web
+          development boot-camp at Academlo and obtained my Google IT Support
+          certification.
+        </p>
+        <div>
+          <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-4" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
