b0d96490c46eaa822d0c0cfb1dc36459ac0f9b32
Osimitzu <angel_aocc@hotmail.com>
2023-10-27 03:42:17 -0600

    work section mobile styles updated

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { RxDotFilled } from "react-icons/rx";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import ProjectItem from "./ProjectItem";
 import SushiProject from "../assets/SushiProject.png";
 import InvitationProject from "../assets/InvitationProject.png";
@@ -67,7 +68,7 @@ const Work = () => {
       className="w-full h-full bg-[#0a192f] text-gray-300 px-4 md:px-20 flex justify-center items-center"
     >
       <section className="flex flex-col justify-center items-center w-full">
-        <section className="w-full mb-12 lg:mb-4">
+        <section className="w-full lg:mb-4">
           <p className="text-2xl md:text-4xl font-bold inline border-b-4 border-pink-600">
             Work
           </p>
@@ -75,20 +76,33 @@ const Work = () => {
         </section>
 
         {/* Card container */}
-        <section className="w-full py-4 flex justify-center items-center flex-col gap-2">
-          {/* Project item */}
-          <ProjectItem
-            // key={index}
-            projectName={projects[index].name}
-            backImg={projects[index].backImg}
-            linkDemo={projects[index].linkDemo}
-            linkCode={projects[index].linkCode}
-            prevFunction={prevProject}
-            nextFunction={nextProject}
-            // slides={projects}
-            // goToSlide={slideDotsF}
-          />
-          <div className="flex top-4 justify-center py-2">
+        <section className="w-full py-4 flex justify-center items-center flex-col gap-2 text-center transition-all duration-300">
+          <h2 className="text-xl font-bol w-full lg:hidden transition-all duration-300">
+            {projects[index].name}
+          </h2>
+          <div className="flex justify-between items-center transition-all duration-300">
+            <div className="w-fit h-fit rounded-full p-2 bg-black/60 text-white cursor-pointer lg:hidden">
+              <BsChevronCompactLeft size={10} onClick={prevProject} />
+            </div>
+            {/* Project item */}
+            <ProjectItem
+              // key={index}
+              projectName={projects[index].name}
+              backImg={projects[index].backImg}
+              linkDemo={projects[index].linkDemo}
+              linkCode={projects[index].linkCode}
+              prevFunction={prevProject}
+              nextFunction={nextProject}
+              // slides={projects}
+              // goToSlide={slideDotsF}
+            />
+            {/* Project item END */}
+            <div className="w-fit h-fit rounded-full p-2 bg-black/60 text-white cursor-pointer lg:hidden">
+              <BsChevronCompactRight size={10} onClick={nextProject} />
+            </div>
+          </div>
+          {/* Dots to slider */}
+          <div className="flex top-4 justify-center py-1">
             {projects.map((project, projectIndex) => (
               <div
                 key={projectIndex}
@@ -100,6 +114,19 @@ const Work = () => {
                 <RxDotFilled className="transition duration-300 ease-in-out hover:text-pink-600" />
               </div>
             ))}
+          </div>
+          {/* Dots to slider END */}
+          <div className="text-center flex justify-center w-full h-fit items-center gap-4 lg:hidden">
+            <a href={projects[index].linkDemo} target="_blank">
+              <button className="text-center rounded-lg px-2 py-1 m-1 bg-black/60 text-white font-bold text-base">
+                Demo
+              </button>
+            </a>
+            <a href={projects[index].linkCode} target="_blank">
+              <button className="text-center rounded-lg px-2 py-1 m-1 bg-black/60 text-white font-bold text-base">
+                Code
+              </button>
+            </a>
           </div>
         </section>
       </section>
